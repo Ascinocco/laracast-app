d63914a632c521118c00a75e930e8bd43c169189
Anthony Scinocco <scinocco.a@gmail.com>
2015-10-27 14:56:49 -0400

    Worked on Gulp

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title></title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css" >
 </head>
 <body>
 <hr>
@@ -14,7 +15,8 @@
 
     <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
-    <script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js"></script>
+    <!--<script>
         //this is needed to use the overlay
         //activate the modal on the id within the flash.blade.php
         //$('#flash-overlay-modal').modal();
@@ -22,7 +24,7 @@
         //if we have a div with a class alert without class alert-important delay 3 seconds then slide up
         //this is not needed if we use the flash package
         //$('div.alert').not('.alert-important').delay(3000).slideUp(300);
-    </script>
+    </script>-->
 
     @yield('footer')
 </body>
--- resources/views/articles/partials/form.blade.php
+++ resources/views/articles/partials/form.blade.php
@@ -20,9 +20,18 @@
     {!! Form::label('tag_list', 'Tags:') !!}
     <!--since multiple tags can be selected we need to specifiy that-->
     <!--tags[] is an array with the [] notation-->
-    {!! Form::select('tag_list[]', $tags, null,['class' => 'form-control', 'multiple']) !!}
+    {!! Form::select('tag_list[]', $tags, null,['id' => 'tag_list','class' => 'form-control', 'multiple']) !!}
 </div>
 
 <div class="form-group">
     {!! Form::submit($submitButtonText, ['class' => 'btn btn-primary form-control']) !!}
 </div>
+
+@section('footer')
+<script>
+    $('#tag_list').select2({
+        placeholder: 'Choose a tag'
+    });
+
+</script>
+@endsection
